d534a4377c680a9e64e5e5490a97377ed4eb6580
Yoga Premium <Yoga Premium@ATIK-LAPTOP>
2023-02-18 10:46:38 +0200

    Add project files.

---
+++ NobuPizza/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace NobuPizza.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public decimal Cost { get; set; }
+		public decimal Discount { get; set; }
+		public decimal Weight { get; set; }
+
+		public decimal Calories { get; set; }
+		public DateTime CreatedOn { get; set; }
+		public bool Availability { get; set; }
+
+		public int Quantity { get; set; }
+
+
+	}
+}
